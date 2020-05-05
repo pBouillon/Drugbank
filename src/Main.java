e23881f0f571031c46fc544f97b66a7973fe2ae6
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-05-05 14:43:21 +0200

    🎯 Add repository initialization in main class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import factory.RepositoryFactorySingleton;
 import repository.DrugRepository;
 
+/**
+ * Program startup class
+ */
 public class Main {
 
+    /**
+     * Program's entry point
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
 
+        // Get an instance of the Drug repository
         DrugRepository drugRepository =  RepositoryFactorySingleton.instance.getDrugRepository();
 
-        System.out.println("Hello World !");
     }
 
 }
