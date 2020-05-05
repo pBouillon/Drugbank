5193b59bf7e211e6544cb4f3c062c49e54c946c2
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-05-05 10:04:08 +0200

    ✨ Create drugbank parser

--- src/dao/drugbank/DrugBankParser.java
+++ src/dao/drugbank/DrugBankParser.java
@@ -6,16 +6,31 @@ import util.IParser;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
 
+/**
+ * Drug bank file parser
+ * @see IParser
+ */
 public class DrugBankParser implements IParser<Drug> {
 
+    /**
+     * Represent the known fields of the parsed file
+     */
     private static class Fields {
 
+        /**
+         * Used when beginning a new drug card
+         */
         public static final String BEGIN_DRUGCARD = "#BEGIN_DRUGCARD";
 
+        /**
+         * Used when the following line will be the drug's generic name
+         */
         public static final String GENERIC_NAME = "# Generic_Name";
 
     }
@@ -24,42 +39,62 @@ public class DrugBankParser implements IParser<Drug> {
      * @inheritDoc
      */
     @Override
-    public List<Drug> extractData(Path source) throws IOException {
-        List<Drug> drugs = new ArrayList<>();
+    public Iterable<Drug> extractData(Path source) throws IOException {
+        Queue<Drug> drugs = new LinkedList<>();
 
+        // Atomic boolean for file parsing
         AtomicBoolean isGenericNameField = new AtomicBoolean(false);
+        AtomicBoolean isNewDrugCardField = new AtomicBoolean(false);
+
+        // Supplier to indicate if any flag is raised
+        BooleanSupplier isAnyFlagRaised = ()
+            -> isGenericNameField.get() || isNewDrugCardField.get();
+
+        // Set the flags according to the currently read field
+        Consumer<String> setFlags = (field) -> {
+            if (field.contains(Fields.BEGIN_DRUGCARD)) {
+                isNewDrugCardField.set(true);
+                return;
+            }
+
+            if (field.contains(Fields.GENERIC_NAME)) {
+                isGenericNameField.set(true);
+            }
+        };
 
         Files.lines(source)
             .forEachOrdered(line -> {
+                // Skip empty lines
                 if (line.isEmpty()) {
-                    resetFlags(isGenericNameField);
                     return;
                 }
 
+                // If no flags are raised try to set them instead of processing
+                if (!isAnyFlagRaised.getAsBoolean()) {
+                    setFlags.accept(line);
+                    return;
+                }
+
+                // Set the generic name of the lastly added Drug
                 if (isGenericNameField.get()) {
-                    drugs.get(drugs.size() - 1).setName(line);
+                    // Drug should not be null here
+                    // If the parsed file is correctly formatted, there should be a new drug card before anything else
+                    // which will create a new Drug instance in the Queue
+                    //noinspection ConstantConditions
+                    drugs.peek()
+                        .setName(line);
+                    isGenericNameField.set(false);
                     return;
                 }
 
-                switch (line) {
-                    case Fields.BEGIN_DRUGCARD:
-                        drugs.add(new Drug());
-                        break;
-
-                    case Fields.GENERIC_NAME:
-                        isGenericNameField.set(true);
-                        break;
+                // Create a new instance of Drug for this new card
+                if (isNewDrugCardField.get()) {
+                    drugs.add(new Drug());
+                    isNewDrugCardField.set(false);
                 }
-
             });
 
         return drugs;
-    }
-
-    private void resetFlags(AtomicBoolean... atomicBooleans) {
-        for (AtomicBoolean atomicBoolean : atomicBooleans) {
-            atomicBoolean.set(false);
-        }
     }
 
 }
--- src/util/IParser.java
+++ src/util/IParser.java
@@ -2,10 +2,19 @@ package util;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
 
+/**
+ * Represent a parser
+ * @param <T> The type of the element to produce on parsing
+ */
 public interface IParser<T> {
 
-    List<T> extractData(Path source) throws IOException;
+    /**
+     * Parse a source and extract the desired elements from it
+     * @param source Source to parse
+     * @return An Iterable of the targeted elements extracted
+     * @throws IOException If the provided path does not exists
+     */
+    Iterable<T> extractData(Path source) throws IOException;
 
 }
