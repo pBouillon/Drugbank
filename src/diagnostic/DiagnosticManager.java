43fe95e50dd0ff04f1ad34983cfcc7ba22e9a987
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-05-18 15:18:02 +0200

    📚 Add class documentation

--- src/diagnostic/DiagnosticManager.java
+++ src/diagnostic/DiagnosticManager.java
@@ -3,10 +3,24 @@ package diagnostic;
 import factory.RepositoryFactory;
 import factory.RepositoryFactorySingleton;
 
+/**
+ * Diagnostic manager entity to perform diagnostic based on response/request POJOs
+ * @see DiagnosticRequest
+ * @see DiagnosticResponse
+ */
 public class DiagnosticManager {
 
-    RepositoryFactory repositoryFactory = RepositoryFactorySingleton.instance;
+    /**
+     * Provider for the various repositories
+     * @see RepositoryFactorySingleton
+     */
+    private RepositoryFactory _repositoryFactory = RepositoryFactorySingleton.instance;
 
+    /**
+     * Core method to generate a diagnostic from a request
+     * @param diagnosticRequest Diagnostic request holding the information on which base the diagnostic process
+     * @return A DiagnosticResponse holding the diagnostic
+     */
     public static DiagnosticResponse generateDiagnostic(DiagnosticRequest diagnosticRequest) {
         return null;
     }
--- src/diagnostic/DiagnosticResponse.java
+++ src/diagnostic/DiagnosticResponse.java
@@ -1,4 +1,8 @@
 package diagnostic;
 
+/**
+ * Container for the diagnostic manager response
+ * @see DiagnosticManager
+ */
 public class DiagnosticResponse {
 }
