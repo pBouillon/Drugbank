7c354043df11b2261a1814b95c8ff9ad06d48b5a
Antoine Jacque <a.jacque.5498@gmail.com>
2020-05-26 16:22:11 +0200

    Minimal CLI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import common.pojo.Disease;
+import common.pojo.Drug;
 import diagnostic.DiagnosticManager;
 import diagnostic.request.DiagnosticRequest;
+import diagnostic.response.DiagnosticResponse;
+import lucene.searcher.LuceneSearcherBase;
+
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Program startup class
@@ -12,11 +19,38 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        System.out.println("Enter the name of each symptoms you want");
+        System.out.println("When you have entered all of the wanted symptoms, type :q to find the result");
+        Scanner sc = new Scanner(System.in);
+        String curr = "";
+        StringBuilder query = new StringBuilder();
+        while(!curr.equals(":q")) {
+            curr = sc.nextLine();
+            query.append(' ');
+            query.append(LuceneSearcherBase.getFieldForLuceneExactSearchOn(curr));
+        }
+
         // Dummy request for pre-prod test purposes
         DiagnosticRequest diagnosticRequest = new DiagnosticRequest();
-        diagnosticRequest.setUndesirableEffect("headache");
+        diagnosticRequest.setUndesirableEffect(query.toString());
 
-        DiagnosticManager.generateDiagnostic(diagnosticRequest);
+        DiagnosticResponse diagnosticResponse = DiagnosticManager.generateDiagnostic(diagnosticRequest);
+
+        List<Disease> diseaseCauses = diagnosticResponse.getDiseaseCauses();
+        System.out.println("-------------------------------------");
+        System.out.println("Possible diseases".toUpperCase());
+        System.out.println("-------------------------------------");
+        for (Disease d: diseaseCauses) {
+            System.out.println(d.getName());
+        }
+        System.out.println("-------------------------------------");
+        System.out.println("Possible drug causes".toUpperCase());
+        System.out.println("-------------------------------------");
+        List<Drug> drugCauses = diagnosticResponse.getDrugCauses();
+        for (Drug d: drugCauses) {
+            System.out.println(d.getName());
+        }
+
     }
 
 }
